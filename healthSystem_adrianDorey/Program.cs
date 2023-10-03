affc62a5b52910df8c049381220ad7500ebf1240
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-10-03 15:14:31 -0300

    added some declarations

--- healthSystem_adrianDorey/Program.cs
+++ healthSystem_adrianDorey/Program.cs
@@ -8,8 +8,49 @@ namespace healthSystem_adrianDorey
 {
     internal class Program
     {
+        // declarations
+        static int health = 100;
+        static int lives = 3;
+
+
+
         static void Main(string[] args)
         {
+
+
         }
+
+        static void showHUD()
+        {
+            Console.WriteLine("Health: " + " / Health Status: ");
+            healthCheck();
+            Console.WriteLine("Lives: ");
+            Console.WriteLine("Shield: ");
+            Console.WriteLine("");
+        }
+
+        static void heal(int hp)
+        {
+
+        }
+
+        static void healthCheck()
+        {
+            Console.WriteLine();
+        }
+
+
+
+        static void regenerateShield(int hp)
+        {
+
+        }
+
+        // modifies health, shield & lives
+        static void takeDamage(int damage)
+        {
+
+        }
+
     }
 }
