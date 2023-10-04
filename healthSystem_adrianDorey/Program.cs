0075dbe968eefa2d74c3dbf89ce7de115a95d0ad
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-10-04 14:19:56 -0300

    ChangedValues

--- healthSystem_adrianDorey/Program.cs
+++ healthSystem_adrianDorey/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,37 +11,85 @@ namespace healthSystem_adrianDorey
     {
         // declarations
         static int health = 100;
+        static string status;
         static int lives = 3;
+        static int shield = 100;
 
 
 
         static void Main(string[] args)
         {
+            showHUD();
 
-
+            takeDamage(-20);
+           
+            Console.WriteLine("Continue");
+            Console.ReadKey();
         }
 
         static void showHUD()
         {
-            Console.WriteLine("Health: " + " / Health Status: ");
             healthCheck();
-            Console.WriteLine("Lives: ");
-            Console.WriteLine("Shield: ");
+            Console.WriteLine("Health: " + health + " / Health Status: " + status); 
+            Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Lives: " + lives);
             Console.WriteLine("");
         }
 
+        // modifies health / based on parameter input(input describes how much player is healed)
+        // range checking / error checking
         static void heal(int hp)
         {
+            Console.WriteLine();
 
+            health = health + hp;
+
+            if (health >= 100)
+            {
+                health = 100;
+            }
         }
 
         static void healthCheck()
         {
-            Console.WriteLine();
+            if (health <= 0)
+            {
+                status = "Player Died";
+                health = 0;
+                lives = lives - 1;
+            }
+            else if ((health > 0) && (health <= 15))
+            {
+                status = "Imminent Danger";
+            }
+            else if ((health > 15) && (health <= 50))
+            {
+                status = "Badly Hurt";
+            }
+            else if ((health > 50 && health <= 75))
+            {
+                status = "Hurt";
+            }
+            else if ((health > 75 && health < 100))
+            {
+                status = "Healthy";
+            }
+            else if (health == 100)
+            {
+                status = "Perfectly Healthy";
+            }
+
+            else if (lives <= 0)
+            {
+                Console.WriteLine("Game Over");
+                lives = 0;
+            }
+
         }
 
 
-
+        // modifies shield /based on parameter input(input describes how much shield should regenerate)
+        // range checking / error checking
         static void regenerateShield(int hp)
         {
 
@@ -49,7 +98,15 @@ namespace healthSystem_adrianDorey
         // modifies health, shield & lives
         static void takeDamage(int damage)
         {
+            if (damage <= 0)
+            {
+                Console.WriteLine("Error: Player cannot take negative value of damage. Must be positive value.");
+                return;
+            }
+            else if(shield < damage)
+            {
 
+            }
         }
 
     }
